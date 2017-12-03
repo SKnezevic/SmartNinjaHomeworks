4a9cb17033f582a398fc4b2421e160dee2bc2559
SKnezevic <sandra.knezevic25@gmail.com>
2017-12-03 13:34:13 +0100

    Domaca zadaca 08

---
+++ domacaZadaca08.py
@@ -0,0 +1,10 @@
+#UPPER TO LOWER AND LOWER TO UPPER
+#Tko je doli sad bi gori...
+
+sentence = raw_input("Write a sentence you want to convert to lowercase: ")
+
+print sentence.lower()
+
+sentence2 = raw_input("Write a sentence you want to convert to uppercase: ")
+
+print sentence2.upper()
